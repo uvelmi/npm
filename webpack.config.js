ac2ab5c9c8cf2ee83ff5620d6496d4dcbd38c58a
Elena Uvarova <uvelmi@mail.ru>
2024-02-26 23:53:46 +0700

    `

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const CopyPlugin = require('copy-webpack-plugin')
-const mode = process.env.NODE_ENV === 'production' ? 'production':'development'
+const mode =process.env.NODE_ENV === 'production' ? 'production' : 'development'
 
 module.exports = {
   entry:'./src/index.js',
@@ -32,4 +32,4 @@ module.exports = {
     }),
     new HtmlWebpackPlugin({
       template: './index.html',
-}), ], }
+}),],}
